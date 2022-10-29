e2e66b9c87a1513748f535680585a99a891dd3f6
kannah bansal <kannahworld@gmail.com>
2022-10-29 12:46:07 -0700

    Json

--- endPoints/src/main/java/restAPI/endPoints/JsonFormats.java
+++ endPoints/src/main/java/restAPI/endPoints/JsonFormats.java
@@ -1,11 +1,13 @@
 package restAPI.endPoints;
 
+import java.util.ArrayList;
+
 public class JsonFormats {
 	//HEHE I HATE JAVA
 }
 
 
-class CreateProfileJson
+class CreateProfileJson extends JsonFormats
 {
 	String first;
 	String last;
@@ -13,4 +15,26 @@ class CreateProfileJson
 	int SexOrient;
 	String insta;
 	String description;
+	Preferences prefered;
+	Rank selfRank;
+	Rank preferRanks;
+}
+
+class Preferences
+{
+	ArrayList<String> people;
+}
+
+class Rank
+{
+	int introverted;
+	int humor;
+	int adventure;
+	int ambition;
+	int artistic;
+	int wOfAff;
+	int physTouch;
+	int gifts;
+	int qualTime;
+	int service;
 }
