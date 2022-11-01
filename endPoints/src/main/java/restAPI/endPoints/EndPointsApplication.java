ccfb3b0d2a1123dbfff1fd30407492a8302cf565
Siddharth Bansal <82485739+KANNAHWORLD@users.noreply.github.com>
2022-10-31 23:02:55 -0700

    removed metadata

--- endPoints/src/main/java/restAPI/endPoints/EndPointsApplication.java
+++ endPoints/src/main/java/restAPI/endPoints/EndPointsApplication.java
@@ -2,10 +2,6 @@ package restAPI.endPoints;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
-//import org.springframework.boot.web.server.*;
-//import org.springframework.boot.*;
-//import java.net.http.*;
-//import java.net.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
