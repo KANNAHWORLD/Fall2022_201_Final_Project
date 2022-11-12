78778999d612b085e2839cd6f8942e60c830238c
Siddharth Bansal <82485739+KANNAHWORLD@users.noreply.github.com>
2022-11-12 11:55:46 -0800

    Updated DataStore

--- endPoints/src/main/java/restAPI/endPoints/DataStore.java
+++ endPoints/src/main/java/restAPI/endPoints/DataStore.java
@@ -5,18 +5,117 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 public class DataStore 
 {
-
-	Connection DBConnection = null;
+	private Connection DBConnection;
+  
+	public static void main(String [] args)
+	{
+		//String URL2 = "jdbc:mysql:/35.185.234.102:3306/test201";
+		//DataStore ss = new DataStore(URL2);
+		
+//		String URL = "jdbc:mysql:///test201?cloudSqlInstance="
+//				+ "utopian-sky-368201:us-west1:test201" // Instance name
+//				+ "&socketFactory=com.google."
+//				+ "cloud.sql.mysql.SocketFactory&"
+//				+ "user=test201" // username
+//				+ "&password=horsehorsepig"; // password
+//		
+//		DataStore s = new DataStore(URL);
+		//ss.createSchema("CREATE SCHEMA testSchema");
+		
+		
+		// ^^ IGNORE ABOVE
+		
+		// Creates a new database by using default constructor.
+		// Deletes the schema, then creates a new one at each instance
+		// might be usefule for testing functionality and writing specific code
+		DataStore ds = new DataStore();
+		
+		//Creates Schema "testSchema"
+		ds.generalQuery("CREATE SCHEMA testSchema");
+		
+		// TODO: add code here for testing
+		
+		ds.generalQuery("CREATE TABLE herro(number INT)");
+		ds.generalQuery("INSERT INTO herro VALUES (50)");
+		
+		//Deletes the Schema
+		ds.generalQuery("DROP SCHEMA testSchema");
+	}
+	
+	// IGNORE FOR NOW
 	DataStore(String URL)
 	{
 		try {
-			DBConnection = DriverManager.getConnection(URL);
+			
+			
+			// jdbc:mysql://google/test201?cloudSqlInstance=utopian-sky-368201:us-west1:test201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=root&password=root
+			
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+//            String instanceConnectionName = "TheActualInstanceName"; 
+//            String databaseName = "BudgetApp";
+//
+//            String IP_of_instance = "35.185.234.102";
+//            String username = "201test";
+//            String password = "horsepig";
+//            String jdbcUrl = String.format("jdbc:mysql://%s/%s?cloudSqlInstance=%s" + 
+//                    "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+//                    IP_of_instance, databaseName, instanceConnectionName);
+            
+            String jdbcurl2 = "jdbc:mysql://google/test201?cloudSqlInstance=utopian-sky-368201:us-west1:test201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=root&password=root";
+
+           // Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            Connection conn = DriverManager.getConnection(jdbcurl2);
+            
+            Statement stat = conn.createStatement();
+            
+            DBConnection = conn;
+           
+//            ResultSet res = stat.executeQuery("Select * from users;");
+//            String result = "";
+//            while (res.next()) 
+//            {
+//                result += (res.getString(1) + " " + res.getString(2) + " " + res.getString(3));
+//            }
+        }
+        catch(Exception e)
+        {
+        	System.out.println("Here");
+            System.out.println(e);
+//            return null;
+        }
+
+
+	}
+
+	DataStore()
+	{
+		
+		
+		// TODO: 
+		// GO TO DBConstants AND CHANGE TO YOUR PERSONAL SQL USERNAMES AND PASSWORDS!!!!!!!
+		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	
+		
+		// Initializes a connection to the database, and assigns it to DBConnection variable
+		// to be use by other member functions
+		try {
+			Connection conn = DriverManager.getConnection
+					("jdbc:mysql://localhost:3306", DBConstants.UserName, DBConstants.Password);
+			DBConnection = conn;
+			conn.createStatement();
+			
 		} catch (SQLException e) {
-			System.out.println("Error connecting to Database!");
+			e.printStackTrace();
 		}
+		
 	}
 	
 	JsonFormats getQuery(String query)
@@ -33,9 +132,6 @@ public class DataStore
 			return jf;
 		}
 		
-		
-		
-		
 		return new JsonFormats();
 	}
 	
@@ -45,5 +141,18 @@ public class DataStore
 		
 		
 		return new JsonFormats();
+	}
+	
+	public void generalQuery(String query)
+	{
+		Statement st = null;
+		try {
+			st = DBConnection.createStatement();
+			st.execute(query);
+		} catch (SQLException e) {
+			System.out.println("SQL STATEMENT FAILED");
+			System.out.println(e.getMessage());
+		}
+		
 	}
 }
